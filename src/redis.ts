6923b7b00981e37f61c2f9b725ae16ecf7812a46
Filip <filip@sierra.team>
2022-10-31 16:24:38 +1100

    clean up

--- src/redis.ts
+++ src/redis.ts
@@ -20,7 +20,7 @@ export class RedisStore implements CandleStore {
   async storePrice(p: PriceData, ts: number): Promise<void> {
     if (p.price !== undefined) {
       await this.client.add(
-        `${this.symbol}-Price`,
+        `${this.symbol}-PERP`,
         ts.toString(),
         p.price.toString(),
         { onDuplicate: "FIRST" }
@@ -39,7 +39,7 @@ export class RedisStore implements CandleStore {
       // Range from-to is inclusive
       // @ts-ignore
       const result: Promise<number[][]> = this.client.range(
-        `${this.symbol}-Price`,
+        `${this.symbol}-PERP`,
         from.toString(),
         to.toString(),
         {
